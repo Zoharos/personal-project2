bbb4fb85c8f6f67772690ee1aeaed955339b4fe0
Zoharos <zorgolan@gmail.com>
2018-12-02 22:59:57 +0200

    Linux

--- app/server/pages/pagesRouter.js
+++ app/server/pages/pagesRouter.js
@@ -9,6 +9,7 @@ router.use(express.json());
 router.get('/*', (req, res) => {
 
     const match = R.contains(req.url, routes);
+    console.log(req.url);
     //routes.reduce((acc, route) => (route == req.url ? true : false) || acc , null);
     if (!match) {
         res.status(404).send('page not found');
